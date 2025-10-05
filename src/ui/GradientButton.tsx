dfcecdcee97d09a32f870d6ed80d06f2aef1922c
oganezovarno2-cpu <oganezovarno2@gmail.com>
2025-10-06 00:00:48 +0100

    Update GradientButton.tsx

--- src/ui/GradientButton.tsx
+++ src/ui/GradientButton.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-export default function GradientButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  const { className="", ...rest } = props
+
+export default function GradientButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  const { className = "", children, ...rest } = props;
   return (
-    <button {...rest}
-      className={`w-full py-3 rounded-2xl btn-grad text-white font-semibold shadow ${className}`} />
-  )
+    <button
+      {...rest}
+      className={`w-full py-3 rounded-2xl btn-grad text-white font-semibold shadow ${className}`}
+    >
+      {children}
+    </button>
+  );
 }

27d9a1797ea22d5ae01dfc3fdf10089fd79cbdf3
oganezovarno2-cpu <oganezovarno2@gmail.com>
2025-10-05 23:43:37 +0100

    Create GradientButton.tsx

---
+++ src/ui/GradientButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function GradientButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { className="", ...rest } = props
+  return (
+    <button {...rest}
+      className={`w-full py-3 rounded-2xl btn-grad text-white font-semibold shadow ${className}`} />
+  )
+}
